d5a0f7da2966a844f35bbf4bce7d4c40f6ca79ef
Egor-record <egor.record@yandex.ru>
2016-01-26 14:04:13 +0300

    Delete create.js

--- builds/dev/js/services/create.js
+++
@@ -1,7 +0,0 @@
-angular.module('EgorFintess')
-  .factory('Channels', function($firebaseArray, FirebaseUrl){
-    var ref = new Firebase(FirebaseUrl+'/channels');
-    var channels = $firebaseArray(ref);
-
-    return channels;
-  });
